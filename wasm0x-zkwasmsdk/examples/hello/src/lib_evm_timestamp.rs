6075979987b9caeed0b482297877b14c2d18e2dc
Stas Stepanov <stanislav@ankr.com>
2023-01-20 15:54:47 +0600

    change main signature

--- wasm0x-zkwasmsdk/examples/hello/src/lib_evm_timestamp.rs
+++ wasm0x-zkwasmsdk/examples/hello/src/lib_evm_timestamp.rs
@@ -2,7 +2,11 @@ use wasm0x_zkwasmsdk::evm_timestamp;
 use wasm0x_zkwasmsdk::Uint64;
 
 #[no_mangle]
-pub extern "C" fn main(_input: i32) -> Uint64 {
+pub extern "C" fn main(_input: i32) -> i32 {
     let ts = evm_timestamp();
-    ts
+    let mut sum = 0;
+    for b in ts {
+        sum += b as i32;
+    }
+    sum
 }
